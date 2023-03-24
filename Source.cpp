e83e5430f574ab2d2b0d91b5bd4496275b5a2cd3
david <david@DESKTOP-QFFHB62>
2023-03-24 21:18:35 +0400

    Добавьте файлы проекта.

---
+++ Source.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+class Road {
+public:
+	double length;
+	int width;
+	Road();
+};
+Road::Road() {
+	length = 555.5;
+	width = 2;
+}
+int main() {
+	using namespace std;
+	cout << "Test v0.1:Ok.\n";
+	Road* road;
+	road = new Road();
+	cout << "Before access: \n";
+	cout << "Length: " << road->length << endl;
+	cout << "Width: " << road->width << endl;
+	road->length = 444.4;
+	road->width = 4;
+
+	cout << "\nAfter access: \n";
+	cout << "Length: " << road->length << endl;
+	cout << "Width: " << road->width << endl;
+	return 0;
+}
